b9601497b7e1b447c5f2d3d2e6f4ac9dfb96a228
NRaghunath <NRaghunath@OABLN709>
2018-10-19 17:34:50 +0530

    fjakfj

--- UnitTestProjecttestmath/UnitTest1.cs
+++ UnitTestProjecttestmath/UnitTest1.cs
@@ -6,8 +6,33 @@ namespace UnitTestProjecttestmath
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test1addition()
         {
+            var controller = new ValuesController();
+            var result = controller.Add(2, 3);
+            Assert.Equals(5, result);
         }
+        [TestMethod]
+        public void Test1subtraction()
+        {
+            var controller = new ValuesController();
+            var result = controller.Subtract(10, 3);
+            Assert.Equals(7, result);
+        }
+        [TestMethod]
+        public void Test1Multiply()
+        {
+            var controller = new ValuesController();
+            var result = controller.Multiply(5, 3);
+            Assert.Equals(15, result);
+        }
+        [TestMethod]
+        public void Test1division()
+        {
+            var controller = new ValuesController();
+            var result = controller.Divide(9, 3);
+            Assert.Equals(3, result);
+        }
+
     }
 }
