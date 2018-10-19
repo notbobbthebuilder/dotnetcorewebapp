b553fe6e78ff059afe40bda70fcf307aa4eb8069
Raghunath <NRaghunath@ad.harman.com>
2018-10-19 15:07:34 +0530

    fddsjfd

---
+++ mathcalci/Controllers/CalculatorController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mathcalci.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+    }
+}
